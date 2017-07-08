eea00b33d391cc0b7027785816335f75259f6013
徐刚文 <xugangwen@xugangwens-Mac-mini.local>
2017-07-08 17:58:54 +0800

    add_test

--- docnote/src/test/java/TestDeadThread.java
+++ docnote/src/test/java/TestDeadThread.java
@@ -2,12 +2,20 @@ package test.java;
 
 import main.thread.DeadThread;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by bbking on 17-7-1.
  */
 public class TestDeadThread {
     public static void main(String[] args) {
         try {
+            ThreadPoolExecutor threadPoolExecutor=new ThreadPoolExecutor(1, 1, 1, TimeUnit.DAYS,
+                    new ArrayBlockingQueue<Runnable>(1));
+            threadPoolExecutor.submit()
             DeadThread t1 = new DeadThread();
             t1.setName("a");
             Thread thread1 = new Thread(t1);
